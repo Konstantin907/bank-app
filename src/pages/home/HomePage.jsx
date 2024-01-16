af4500aba1a7f05d4b6573cbc0dc0bcdbd592464
konstantin <konstantinivanovic12345@gmail.com>
2024-01-15 17:49:11 -0800

    footer added

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,6 +3,8 @@ import Header from '../../components/header/Header'
 import HeroSection from '../../components/hero/HeroSection'
 import { LineElements } from '../../components/lineElements/LineElements'
 import { WhatPeopleThink } from '../../components/whatPeopleThink/WhatPeopleThink'
+import { Footer } from '../../components/footer/Footer'
+import { Copyrights } from '../../components/copyrights/Copyrights'
 
 export default function HomePage() {
   return (
@@ -11,6 +13,8 @@ export default function HomePage() {
         <HeroSection />
         <LineElements /> 
         <WhatPeopleThink />
+          <Footer />
+          <Copyrights />
     </>
   )
 }
