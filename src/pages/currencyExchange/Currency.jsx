7d861a810f4082b0812bb5ffd7f0c5f6849a6c0b
konstantin <konstantinivanovic12345@gmail.com>
2024-03-26 19:59:33 -0700

    polishing page

--- src/pages/currencyExchange/Currency.jsx
+++ src/pages/currencyExchange/Currency.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import "./currency.scss"
 import dotenv from "dotenv"
-
+import { LineElements } from '../../components/lineElements/LineElements';
 
 export const Currency = () => {
 
@@ -30,6 +30,7 @@ export const Currency = () => {
 }, [sourceCurrency, targetCurrency, sourceAmount]);
 
 console.log(import.meta.env.REACT_APP_API_KEY);
+
 
 const handleSourceAmountChange = (e) => {
   const newAmount = e.target.value;
@@ -77,11 +78,41 @@ const handleTargetCurrencyChange = (e) => {
                 ))}
             </select>
            </div>
-           
-          
+                 
         </div>
       </div>
-        
+
+      
+      
+
+      <div className="rest">
+        <h1>What is curreny exchange?</h1>
+        <h3>
+            Currency exchange, or forex trading, involves the buying and selling of currencies on the foreign exchange market with the aim of making a profit. 
+            The value of currencies on this market fluctuates constantly due to a variety of factors.
+
+            One of the primary drivers of currency value changes is supply and demand. When more people or businesses want a particular currency, its value goes up. Conversely, if there's less demand for a currency, its value goes down. For example, if a country's economy is strong, businesses and investors might want more of that country's currency to invest or make purchases there, driving up its value.
+
+
+        </h3>
+        <br /> 
+        <h3>
+        Economic indicators also play a crucial role. These include reports such as employment rates, inflation, manufacturing output, and retail sales. 
+        Strong economic performance in a country tends to increase the value of its currency because it suggests future interest rate hikes, attracting investors looking for higher returns.
+        </h3>
+
+        <br />
+
+        <h3>
+        Interest rates set by central banks are another major factor. Higher interest rates offer lenders in an economy a higher return relative to other countries. Therefore, higher interest rates attract foreign capital and cause the exchange rate to rise. Conversely, when a country reduces its interest rates, its currency might weaken as investors seek better returns elsewhere.
+
+        </h3>
+        <br />
+        <h3>
+        Political stability and economic performance can affect currency strength. Countries with less risk for political turmoil are more attractive to foreign investors. As a result, these countries tend to have stronger currencies.
+        </h3>
+      </div>
+         
     </div>
   )
 }
